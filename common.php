6821bbe96be1eda14ce4246b73ab9a7eee2bdf8d
unknown <liujinghaiok@gmail.com>
2015-04-28 16:52:22 +0800

    入口文件

---
+++ common.php
@@ -0,0 +1,35 @@
+<?php
+/**
+* 全站的统一的入口文件,方便其他的文件调用
+*/
+class common {
+	/**
+	* 加载类库文件
+	*/
+	public function load_class() {
+
+	}
+
+	/**
+	* 加载文件
+	* @param $classname   文件名
+	* @param $path        文件路径
+	*/
+	public function load($classname) {
+
+	}
+
+	/**
+	* 处理用户提交的参数
+	*/
+	public function param() {
+
+	}
+
+	/**
+	* 错误日志
+	*/
+	public function log() {
+		
+	}
+}
--- index.php
+++ index.php
@@ -3,3 +3,5 @@
 * landphp的入口文件
 * @author 彭城岛主
 */
+define('landphp',true);
+
